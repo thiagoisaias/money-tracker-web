ef2a9b3be8d87dd6c9c26afc946e908379c2164e
Thiago <thiagoisaiasbrg@gmail.com>
2018-04-24 18:56:52 -0300

    Add auth redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Component, Fragment } from "react";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 import Home from "../Home/Home";
-import Layout from "../Layout/Layout";
 import SignupPage from "../SignupPage/SignupPage";
 import TransactionForm from "../TransactionForm/TransactionForm";
 
 class App extends Component {
   render() {
+    let authRedirect = null;
+    if (!this.props.isAuthenticated) {
+      authRedirect = <Redirect to="/auth" />;
+    }
+
     return (
-      <Switch>
-        <Route exact path="/login" component={SignupPage} />
-        <Layout>
+      <Fragment>
+        {authRedirect}
+        <Switch>
+          <Route exact path="/auth" component={SignupPage} />
           <Route exact path="/" component={Home} />
           <Route exact path="/transaction" component={TransactionForm} />
-        </Layout>
-      </Switch>
+          <Route component={SignupPage} />
+        </Switch>
+      </Fragment>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(App));
