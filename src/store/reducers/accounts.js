68c4b2b53be1aea7b9817c2905f0505ce8e31a7b
Thiago <thiagoisaiasbrg@gmail.com>
2018-05-04 20:23:09 -0300

    Create account reducer

---
+++ src/store/reducers/accounts.js
@@ -0,0 +1,88 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+  error: null,
+  isLoading: false,
+  accountList: []
+};
+
+const accounts = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.CREATE_ACCOUNT_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionTypes.CREATE_ACCOUNT_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        isLoading: false,
+        accountList: [...state.accountList, action.accountData]
+      };
+    case actionTypes.CREATE_ACCOUNT_FAIL:
+      return {
+        ...state,
+        errors: action.errors,
+        isLoading: false
+      };
+    case actionTypes.FETCH_ACCOUNTS_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionTypes.FETCH_ACCOUNTS_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        isLoading: false,
+        accountList: action.accountList
+      };
+    case actionTypes.FETCH_ACCOUNTS_FAIL:
+      return {
+        ...state,
+        error: action.error,
+        isLoading: false
+      };
+    case actionTypes.UPDATE_ACCOUNT_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionTypes.UPDATE_ACCOUNT_SUCCESS:
+      // TODO: depends on the account ID
+      return {
+        ...state,
+        error: null,
+        isLoading: false
+      };
+    case actionTypes.UPDATE_ACCOUNT_FAIL:
+      return {
+        ...state,
+        error: action.error,
+        isLoading: false
+      };
+    case actionTypes.DELETE_ACCOUNT_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionTypes.DELETE_ACCOUNT_SUCCESS:
+      // TODO: depends on the account ID
+      return {
+        ...state,
+        error: null,
+        isLoading: false
+      };
+    case actionTypes.DELETE_ACCOUNT_FAIL:
+      return {
+        ...state,
+        error: action.error,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+};
+
+export default accounts;
