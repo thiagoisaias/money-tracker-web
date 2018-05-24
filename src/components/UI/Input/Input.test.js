1a3636e3d5fe8d4e2d28cd5c458b12f4458440e3
Thiago <thiagoisaiasbrg@gmail.com>
2018-05-24 16:44:10 -0300

    Add generic form components

---
+++ src/components/UI/Input/Input.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import Input from "./Input";
+
+describe("Input", () => {
+  const mockProps = {
+  };
+  const component = shallow(<Input {...mockProps} />);
+
+  it("renders properly", () => {
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
